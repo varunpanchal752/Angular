6cc9eae043b18d19154345aefb1456ce45cc7842
Varun Panchal <varunpanchal752@gmail.com>
2025-07-03 01:49:28 +0530

    changes

--- learning-ng/src/app/components/todo-item/todo-item.ts
+++ learning-ng/src/app/components/todo-item/todo-item.ts
@@ -1,13 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlightCompletedTodo } from '../../directives/highlight-completed-todo';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [HighlightCompletedTodo],
+  imports: [HighlightCompletedTodo, UpperCasePipe],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.scss'
 })
 export class TodoItem {
   todo = input.required<Todo>();
+  todoToggled = output<Todo>();
+
+  todoClicked() {
+    this.todoToggled.emit(this.todo());
+  }
 }
